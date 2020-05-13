2e310c54bc0c359b3cfdb68dbd22c2cee420b890
yanhui <yanhui@micous.com>
2020-05-13 15:49:26 +0800

    Initial commit

---
+++ src/main/java/com/lsy/learn/projectlsy/ProjectLsyApplication.java
@@ -0,0 +1,13 @@
+package com.lsy.learn.projectlsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLsyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectLsyApplication.class, args);
+    }
+
+}
