16c0b4f2423dc02aab1ad876c076fb482046a2d0
yanhui <yanhui@micous.com>
2020-05-14 14:46:24 +0800

    多环境配置  jdbc

---
+++ src/main/java/com/lsy/learn/projectlsy/config/MySpringJdbcConfig.java
@@ -0,0 +1,48 @@
+package com.lsy.learn.projectlsy.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@EnableTransactionManagement
+@ComponentScan
+class ApplicationConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DriverManagerDataSource dataSource) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
+}
--- src/main/java/com/lsy/learn/projectlsy/controller/MyController1.java
+++ src/main/java/com/lsy/learn/projectlsy/controller/MyController1.java
@@ -1,4 +1,14 @@
 package com.lsy.learn.projectlsy.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/myController1")
 public class MyController1 {
+
+    @RequestMapping("/func1")
+    public void func1(){
+        System.out.println("MyController1------------------");
+    }
 }
