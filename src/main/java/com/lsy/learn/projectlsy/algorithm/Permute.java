a8ccebcc7d9bdced23262bd2d7d2bed15147f356
yanhui <yanhui@micous.com>
2021-03-08 17:31:35 +0800

    全排列算法

---
+++ src/main/java/com/lsy/learn/projectlsy/algorithm/Permute.java
@@ -0,0 +1,38 @@
+package com.lsy.learn.projectlsy.algorithm;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Yanhui
+ * @date 2021/3/8 15:55
+ */
+public class Permute {
+
+    public static void main(String[] args) {
+        new Permute().permute(new int[]{1, 2, 3});
+    }
+
+    private void permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums, new ArrayList<>(), res);
+
+    }
+
+    private void dfs(int[] nums, List<Integer> list, List<List<Integer>> res) {
+        if (list.size() == nums.length) {
+            res.add(list);
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (list.contains(nums[i])) {
+                continue;
+            }
+            list.add(nums[i]);
+            dfs(nums, list, res);
+            list.remove(list.size() - 1);
+        }
+    }
+}
+
